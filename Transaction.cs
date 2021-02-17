06945895808b038c0930e31babff682712f59083
Benjamin Sierra <bsierra1@byu.edu>
2021-02-17 14:13:23 -0700

    Add project files.

---
+++ Transaction.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using Newtonsoft.Json;
+using EllipticCurve;
+
+namespace RootCoin
+{
+    class Transaction
+    {
+        public PublicKey FromAddress { get; set; }
+        public PublicKey ToAddress { get; set; }
+        public decimal Amount { get; set; }
+        public Signature Signature { get; set; }
+        public Transaction(PublicKey fromAddress, PublicKey toAddress, decimal amount)
+        {
+            this.FromAddress = fromAddress;
+            this.ToAddress = toAddress;
+            this.Amount = amount;
+        }
+
+        public void SignTransaction(PrivateKey signingKey)
+        {
+            string fromAddresDER = BitConverter.ToString(FromAddress.toDer()).Replace("-", "");
+            string signingDER = BitConverter.ToString(signingKey.publicKey().toDer()).Replace("-", "");
+
+            if (fromAddresDER != signingDER)
+            {
+                throw new Exception("You cannot sign transactions for other wallets!");
+            }
+
+            string txHash = this.CalculateHash();
+            this.Signature = Ecdsa.sign(txHash, signingKey);
+        }
+
+        public string CalculateHash()
+        {
+            string fromAddresDER = BitConverter.ToString(FromAddress.toDer()).Replace("-", "");
+            string toAddresDER = BitConverter.ToString(ToAddress.toDer()).Replace("-", "");
+            string transactionData = fromAddresDER + toAddresDER + Amount;
+            byte[] tdBytes = Encoding.ASCII.GetBytes(transactionData);
+            return BitConverter.ToString(SHA256.Create().ComputeHash(tdBytes)).Replace("-", "");
+        }
+
+        public bool IsValid()
+        {
+            if (this.FromAddress is null) return true;
+
+            if (this.Signature is null)
+            {
+                throw new Exception("No signiture in this transaction.");
+            }
+            return Ecdsa.verify(this.CalculateHash(), this.Signature, this.FromAddress);
+        }
+    }
+}
